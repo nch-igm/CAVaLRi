5c85ccd011cd8058a10e25a265129452d47b8994
Rob Schuetz <schuetz.12@osu.edu>
2022-11-21 16:55:42 -0500

    Updating dependencies to rely partially on conda

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,23 +1,23 @@
 # Check to see if dependency path is provided
 dependency_path=${1-dependencies}
 
-# Download bcftools
-wget https://github.com/samtools/bcftools/releases/download/1.16/bcftools-1.16.tar.bz2 -P -P $dependency_path
-tar -xjf $dependency_path/bcftools-1.16.tar.bz2 -C $dependency_path
-cd $dependency_path/bcftools-1.16
-./configure --prefix=$dependency_path
-make
-make install
-cd $dependency_path
+# # Download bcftools
+# wget https://github.com/samtools/bcftools/releases/download/1.16/bcftools-1.16.tar.bz2 -P $dependency_path
+# tar -xjf $dependency_path/bcftools-1.16.tar.bz2 -C $dependency_path
+# cd $dependency_path/bcftools-1.16
+# ./configure --prefix=$dependency_path
+# make
+# make install
+# cd $dependency_path
 
-# Download htslib
-wget https://github.com/samtools/htslib/releases/download/1.16/htslib-1.16.tar.bz2 -P -P $dependency_path
-tar -xjf $dependency_path/htslib-1.16.tar.bz2 -C $dependency_path
-cd $dependency_path/htslib-1.16
-./configure --prefix=$dependency_path
-make
-make install
-cd $dependency_path
+# # Download htslib
+# wget https://github.com/samtools/htslib/releases/download/1.16/htslib-1.16.tar.bz2 -P $dependency_path
+# tar -xjf $dependency_path/htslib-1.16.tar.bz2 -C $dependency_path
+# cd $dependency_path/htslib-1.16
+# ./configure --prefix=$dependency_path
+# make
+# make install
+# cd $dependency_path
 
 # Download annovar databases
 dependencies/annovar/annotate_variation.pl -downdb -buildver hg38 -webfrom annovar refGene $dependency_path/human_db
