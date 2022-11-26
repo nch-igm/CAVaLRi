fcdd626475e6ac1e7ea2a300ed260e19121fe443
Rob Schuetz <schuetz.12@osu.edu>
2022-11-26 00:50:52 -0500

    BL-182 Updated post-test probability calculation

--- src/case/methods/calculate_compositeLR.py
+++ src/case/methods/calculate_compositeLR.py
@@ -1,5 +1,13 @@
 import math
 import json
+from scipy.stats import norm, skewnorm
+
+def get_diagnostic_probability(cLR, prior, pos_mu, pos_std, a, loc, scale):
+    pos_prob = norm.pdf(cLR, pos_mu, pos_std)
+    neg_prob = skewnorm.pdf(cLR, a, loc, scale)
+    odds = (pos_prob/neg_prob) * prior
+    probability = odds / (1 + odds)
+    return probability
 
 def calculate_compositeLR(case):
 
@@ -30,9 +38,18 @@ def calculate_compositeLR(case):
         })
 
         # Calculate post-test probability
-        numerator = d['pretest_probability'] * 10**d['compositeLR']
-        denominator = (1 - d['pretest_probability']) + d['pretest_probability'] * 10**d['compositeLR']
-        posttest_probability = numerator / denominator
+        # numerator = d['pretest_probability'] * 10**d['compositeLR']
+        # denominator = (1 - d['pretest_probability']) + d['pretest_probability'] * 10**d['compositeLR']
+        # posttest_probability = numerator / denominator
+        posttest_probability = get_diagnostic_probability(
+                cLR = d['compositeLR'], 
+                prior = 0.006708, 
+                pos_mu = 10.47,
+                pos_std = 8.21,
+                a = 5.18,
+                loc = -17.47,
+                scale = 9.9
+            )
 
         # Append post test probability
         d.update({'postTestProbability': posttest_probability})
