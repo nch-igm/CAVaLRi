207c1a312bc350cc6323fd8ee5e9efbf9a4eea5f
Rob Schuetz <schuetz.12@osu.edu>
2023-10-03 16:05:48 -0400

    Include handling of diseases with multiple gene associations

--- src/case/methods/add_rankings.py
+++ src/case/methods/add_rankings.py
@@ -15,8 +15,6 @@ def add_rankings(case):
         for d, d_data in g_data.items():
             if not re.search('gene_data', d):
 
-                print(d, d_data)
-
                 # Create a dataframe to sort by postTestProbability
                 rank_df = pd.concat([
                     rank_df,
@@ -31,7 +29,7 @@ def add_rankings(case):
     gene_rank_df = rank_df[['geneId', 'postTestProbability']].groupby('geneId').max().reset_index().sort_values(by='postTestProbability', ascending=False)
     gene_rank_df = gene_rank_df.reset_index(drop=True).reset_index().rename(columns=({'index': 'geneRank'})).drop(columns=['postTestProbability'])
     disease_rank_df = rank_df[['omimId', 'postTestProbability']].drop_duplicates().sort_values(by='postTestProbability', ascending=False).reset_index(drop=True).reset_index().rename(columns=({'index': 'diseaseRank'})).drop(columns=['postTestProbability'])
-    rank_df = rank_df.merge(gene_rank_df).merge(disease_rank_df)
+    rank_df = rank_df.sort_values(by='postTestProbability', ascending=False).reset_index(drop=True).merge(gene_rank_df).merge(disease_rank_df)
 
     # Increment rank columns
     rank_df['geneRank'] += 1
@@ -42,7 +40,7 @@ def add_rankings(case):
         for d, d_data in g_data.items():
             if not re.search('gene_data', d):
 
-                gene_rank = int(rank_df.loc[rank_df['omimId'] == d, 'geneRank'].reset_index(drop=True)[0])            
+                gene_rank = int(rank_df.loc[rank_df['geneId'] == g, 'geneRank'].reset_index(drop=True)[0])            
                 disease_rank = int(rank_df.loc[rank_df['omimId'] == d, 'diseaseRank'].reset_index(drop=True)[0])
 
                 d_data['geneRank'] = gene_rank # Step can be duplicated, but that's okay
